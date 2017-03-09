ccf0befd1ee9f00079c964ff5233dd2e6e9e9911
purpletianjing <tianjingsun@outlook.com>
2017-03-09 09:44:38 +0800

    modify Robot to Roboto

--- app/assets/javascripts/ckeditor/ckeditor.js
+++ app/assets/javascripts/ckeditor/ckeditor.js
@@ -13547,7 +13547,7 @@
             }
         })
     })();
-    CKEDITOR.config.font_names = 'Arial/Arial, Helvetica, sans-serif;Comic Sans MS/Comic Sans MS, cursive;Courier New/Courier New, Courier, monospace;Georgia/Georgia, serif;Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;Tahoma/Tahoma, Geneva, sans-serif;Times New Roman/Times New Roman, Times, serif;Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;Verdana/Verdana, Geneva, sans-serif;Robot/"Roboto", sans-serif';
+    CKEDITOR.config.font_names = 'Arial/Arial, Helvetica, sans-serif;Comic Sans MS/Comic Sans MS, cursive;Courier New/Courier New, Courier, monospace;Georgia/Georgia, serif;Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;Tahoma/Tahoma, Geneva, sans-serif;Times New Roman/Times New Roman, Times, serif;Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;Verdana/Verdana, Geneva, sans-serif;Roboto/"Roboto", sans-serif';
     CKEDITOR.config.font_defaultLabel = '';
     CKEDITOR.config.font_style = {
         element: "span",
